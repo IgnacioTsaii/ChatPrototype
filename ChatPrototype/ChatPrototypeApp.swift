825ffc253e2e155914a8b6d90e0dfa92e1d997b3
Ignacio <igu99.tsai@gmail.com>
2024-04-18 09:50:06 -0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Ignacio Tsai on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatPrototype
+//
+//  Created by Ignacio Tsai on 18/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
